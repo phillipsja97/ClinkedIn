ba5a43a213edf63b0ed01ea8a545b8ebc79a78e3
Ivan Phelps <ivanniothemannio@gmail.com>
2020-04-07 18:45:43 -0500

    setup repository and controller, add clinker complete

---
+++ ClinkedIn/Controllers/ClinkersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ClinkedIn.DataAccess;
+using ClinkedIn.Models;
+
+namespace ClinkedIn.Controllers
+{
+    [Route("api/clinkers")]
+    [ApiController]    
+    public class ClinkersController : ControllerBase
+    {
+        ClinkerRepository _repository = new ClinkerRepository();
+
+        [HttpPost]
+        public IActionResult AddClinker(Clinker clinkerToAdd)
+        {
+            _repository.AddClinker(clinkerToAdd);
+            return Created($"Added Clinker {clinkerToAdd.Name}", clinkerToAdd);
+        }
+        
+    }
+}
